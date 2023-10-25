5d7a7e0e507e7a513547522076d2a6d3fa10a0e7
hafizsyedirfanali <hafizsyedirfanali@gmail.com>
2023-10-25 23:44:20 +0530

    update

---
+++ WMSBrokerProject/Models/TaskIndicationRequestModel.cs
@@ -0,0 +1,22 @@
+﻿namespace WMSBrokerProject.Models;
+
+public class TaskIndicationRequestModel
+{
+    public Header header { get; set; }
+    public string inId { get; set; }
+}
+
+public class Header
+{
+    public From from { get; set; }
+    public int updateCount { get; set; }
+    public DateTime created { get; set; }
+    public string priority { get; set; }
+}
+
+public class From
+{
+    public string orgId { get; set; }
+    public string systemId { get; set; }
+}
+
---
+++ WMSBrokerProject/Models/WMSBrokerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WMSBrokerProject.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WMSBrokerController : ControllerBase
+    {
+        private string inId;
+        [HttpPost]
+        public async Task<IActionResult> TaskIndication(TaskIndicationRequestModel model)
+        {
+            inId = model.inId;
+            return Ok();
+        }
+    }
+}
